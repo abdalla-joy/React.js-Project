dd5a6f24a1e5dd45c3542264ced0b1cd4b51085f
abalidecosta@gmail.com <abalidecosta@gmail.com>
2024-07-03 10:22:23 +0300

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return(
+    <div className='footer'>
+        <p>Copyright @ Cryptoplae + Abdalla - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer;
